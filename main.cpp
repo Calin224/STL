e83485671700cd552fec8478065b45ecc42f2c6a
Calin224 <sanducalinm@gmail.com>
2024-10-29 17:48:07 +0200

    Homework#1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -10,23 +11,40 @@ int main()
 
     int no_problems, no_doctors;
     string name, speciality;
-    
-    inFile >> no_problems;
 
-    for (int i = 0; i < no_problems; i++)
-    {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+    // solve
+    vector<string> problems;
+    vector<string> names;
+    vector<string> doctors;
+
+    inFile>>no_problems;
+
+    for(int i = 0; i < no_problems; ++i) {
+        inFile>>name>>speciality;
+        problems.push_back(speciality);
+        names.emplace_back(name);
     }
 
-    inFile >> no_doctors;
+    inFile>>no_doctors;
 
-    for (int i = 0; i < no_doctors; i++)
-    {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+    for(int i = 0; i < no_doctors; ++i) {
+        inFile >> name >> speciality;
+        doctors.push_back(speciality);
+    }
+
+    for(int i = 0; i < problems.size(); ++i) {
+        int ok = 0;
+        for(int j = 0; j < doctors.size(); ++j) {
+            if(problems[i] == doctors[j]) {
+                ok = 1;
+            }
+        }
+
+        if(ok == 1) {
+            cout<<names[i]<<" Accept"<<'\n';
+        }else {
+            cout<<names[i]<<" Reject"<<'\n';
+        }
     }
 
     return 0;
