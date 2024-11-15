9bf2510ea98ab446aea3437f5d07c2a95fb07136
Sandu Calin <63157791+Calin224@users.noreply.github.com>
2024-11-15 19:03:54 +0200

    Problema_bonus

--- main.cpp
+++ main.cpp
@@ -1,32 +1,79 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-
+#include<bits/stdc++.h>
+#include<fstream>
 using namespace std;
 
-int main()
-{
-    ifstream inFile("input.txt");
+#define WORK 9;
 
-    int no_problems, no_doctors;
-    string name, speciality;
-    
+class Problem {
+public:
+    string name;
+    string speciality;
+    int time;
+
+    Problem(string a, string b, int c): name(a), speciality(b), time(c) {};
+};
+
+class Doctor {
+public:
+    string id;
+    string speciality;
+    int time_worked = 0;
+    vector<Problem> problems_solved;
+
+    Doctor(string a, string b): id(a), speciality(b) {};
+};
+
+int main() {
+    ifstream inFile("input4_bonus.txt");
+
+    int no_problems, no_doctors, time_per_problem;
+    string name, speciality, id;
+
+    vector<Problem> problems;
+    vector<Doctor> doctors;
+
     inFile >> no_problems;
 
     for (int i = 0; i < no_problems; i++)
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> time_per_problem;
+
+        Problem problem(name, speciality, time_per_problem);
+        problems.push_back(problem);
     }
 
     inFile >> no_doctors;
 
     for (int i = 0; i < no_doctors; i++)
     {
-        inFile >> name;
+        inFile >> id;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+
+        Doctor doctor(id, speciality);
+        doctors.push_back(doctor);
+    }
+
+    for(auto& problem: problems) {
+        auto doctor = find_if(doctors.begin(), doctors.end(), [problem](const Doctor& doctor) {
+            return doctor.speciality == problem.speciality and doctor.time_worked + problem.time < WORK;
+        });
+
+        if(doctor != doctors.end()) {
+            doctor->time_worked += problem.time;
+            doctor->problems_solved.push_back(problem);
+        }
+    }
+
+    for(auto& doctor: doctors) {
+        if(!doctor.problems_solved.empty()) {
+            cout<<doctor.id<<" "<<doctor.problems_solved.size()<<" ";
+            for(auto& problem: doctor.problems_solved) {
+                cout<<problem.name<<" ";
+            }
+            cout<<"\n";
+        }
     }
 
     return 0;
